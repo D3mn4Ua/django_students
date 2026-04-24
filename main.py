5ad19b9a98b95ff2092524bd768da465a0efaa1d
D3mn4Ua <damndavidkolupatelnosa@gmail.com>
2026-04-24 22:24:34 +0300

    Added students and classes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 django.setup()
 
-from students.models import Teacher, Subject
+from students.models import Teacher, Subject, Class, Student
 
 teacher = Teacher.objects.create(
     name="Teacher1"
@@ -17,3 +17,15 @@ subject = Subject.objects.create(
 )
 
 subject.teachers.add(teacher, teacher1)
+
+student = Student.objects.create(
+    name="Student1"
+)
+student1 = Student.objects.create(
+    name="Student2"
+)
+classes = Class.objects.create(
+    title="Class_number_one"
+)
+
+classes.students.add(student, student1)
--- students/models.py
+++ students/models.py
@@ -6,3 +6,12 @@ class Teacher(models.Model):
 class Subject(models.Model):
     title = models.CharField(max_length=63)
     teachers = models.ManyToManyField(Teacher, related_name="subjects")
+
+###
+
+class Student(models.Model):
+    name = models.CharField(max_length=63)
+
+class Class(models.Model):
+    title = models.CharField(max_length=63)
+    students = models.ManyToManyField(Student, related_name="classes")
